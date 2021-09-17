fa907fa0b040a1cfd49356d3462e0323296f4837
romanpr <roman.priscepov@hotmail.com>
2021-09-17 18:51:11 +0200

    Add comment with url

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ new Autocomplete(document.getElementById("state"), {
 
 // Github Users
 new Autocomplete(document.getElementById("gh-user"), {
+	//url: "https://api.github.com/search/users?q=romanpr&per_page=5",
 	urlFactory: (query, limit) => {
 		return `https://api.github.com/search/users?q=${query}&per_page=${limit}`
 	},
